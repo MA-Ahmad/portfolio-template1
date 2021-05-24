b80968eac8b10dc5cf1b3e6289006a689469a47f
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-24 08:19:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import * as React from 'react';
+import { ChakraProvider, Box, theme } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Home from './components/Home';
 
-function App() {
+export const App = () => {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
+      <Box p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <Home />
       </Box>
     </ChakraProvider>
   );
-}
-
-export default App;
+};
